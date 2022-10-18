38c7dac34b25c98caa4c23695dda386721d1ce57
cesarlopezt <cesarlopez2808@gmail.com>
2022-10-18 10:44:02 -0400

    Adds basic storybook layout

---
+++ CardShuffler/CardSelectionVC.swift
@@ -0,0 +1,29 @@
+//
+//  CardSelectionVC.swift
+//  CardShuffler
+//
+//  Created by Cesar Lopez on 10/18/22.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
